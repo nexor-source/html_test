9b8d42ccdbd6821c60d48ee6b0c5e3b61187e250
Nameless <76464536+nexor-source@users.noreply.github.com>
2024-05-19 02:44:05 +0800

    split html to 3 file

---
+++ script.js
@@ -0,0 +1,28 @@
+function calculateInverse() {
+    // 获取矩阵元素的值
+    var a11 = parseFloat(document.getElementById('a11').value);
+    var a12 = parseFloat(document.getElementById('a12').value);
+    var a13 = parseFloat(document.getElementById('a13').value);
+    var a21 = parseFloat(document.getElementById('a21').value);
+    var a22 = parseFloat(document.getElementById('a22').value);
+    var a23 = parseFloat(document.getElementById('a23').value);
+    var a31 = parseFloat(document.getElementById('a31').value);
+    var a32 = parseFloat(document.getElementById('a32').value);
+    var a33 = parseFloat(document.getElementById('a33').value);
+    
+    // 计算矩阵的行列式
+    var determinant = a11 * (a22 * a33 - a23 * a32) - a12 * (a21 * a33 - a23 * a31) + a13 * (a21 * a32 - a22 * a31);
+
+    // 计算逆矩阵
+    var inverseMatrix = [
+        [(a22 * a33 - a23 * a32) / determinant, (a13 * a32 - a12 * a33) / determinant, (a12 * a23 - a13 * a22) / determinant],
+        [(a23 * a31 - a21 * a33) / determinant, (a11 * a33 - a13 * a31) / determinant, (a13 * a21 - a11 * a23) / determinant],
+        [(a21 * a32 - a22 * a31) / determinant, (a12 * a31 - a11 * a32) / determinant, (a11 * a22 - a12 * a21) / determinant]
+    ];
+
+    // 显示结果
+    document.getElementById('result').innerHTML = "Inverse Matrix: <br>" +
+        inverseMatrix[0].join(' ') + "<br>" +
+        inverseMatrix[1].join(' ') + "<br>" +
+        inverseMatrix[2].join(' ');
+}
